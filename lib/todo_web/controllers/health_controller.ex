fde56dbb2c6b33da8de2d27ebe98171b66f62550
Isaac Priestley <progressions@gmail.com>
2019-02-26 14:05:51 -0500

    Health check controller

--- lib/todo_web/controllers/health_controller.ex
+++ lib/todo_web/controllers/health_controller.ex
@@ -6,7 +6,30 @@ defmodule TodoWeb.HealthController do
   import TodoWeb.ErrorHelpers
 
   def check(conn, _params) do
-    text(conn, "OK")
+    results = results() |> Jason.encode()
+
+    case database_check do
+      :ok -> respond(conn, "healthcheck passed", 204, results)
+      :error -> respond(conn, "healthcheck failed", 500, results)
+    end
+  end
+
+  defp results do
+    # results = {status_code, %{ "X-Todo-Version" => 1.0, "X-Environment" => Mix.env() }, []}
+    "OK"
+  end
+
+  defp respond(conn, message, code, results) do
+    text(conn, message)
+  end
+
+  defp database_check do
+    try do
+      Ecto.Adapters.SQL.query(Todo.Repo, "select 1", [])
+      :ok
+    rescue
+      DBConnection.ConnectionError -> :error
+    end
   end
 
 end
