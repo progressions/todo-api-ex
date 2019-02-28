f8d27d0d3aef14458c434945ff453d0aa486b054
Isaac Priestley <progressions@gmail.com>
2019-02-28 10:38:17 -0500

    Add configuration for swagger file

--- config/config.exs
+++ config/config.exs
@@ -37,6 +37,14 @@ config :logger, :log_file,
   metadata: [:request_id],
   path: "log/#{Mix.env()}.log"
 
+config :todo, :phoenix_swagger,
+  swagger_files: %{
+    "priv/static/swagger.json" => [
+      router: TodoWeb.Router,     # phoenix routes will be converted to swagger paths
+      endpoint: TodoWeb.Endpoint  # (optional) endpoint config used to set host, port and https schemes.
+    ]
+  }
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
